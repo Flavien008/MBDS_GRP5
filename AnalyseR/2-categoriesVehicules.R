7a815a99758fe6e19f82922a5325f56ee3ef2254
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-05-18 21:38:03 +0300

    catégorisation de données en utilisant cluster

--- AnalyseR/2-categoriesVehicules.R
+++ AnalyseR/2-categoriesVehicules.R
@@ -2,31 +2,71 @@
 
   # Nous allons catégoriser les véhicules en cinq catégories principales en fonction de certaines caractéristiques pertinentes, à savoir :
   
-  #citadine - courte et pas beaucoup de cheveaux
-  
-  #sportive - beaucoup de cheveaux & 3 portes
-  
-  #familiale - 7 places + pas puissante et très longue / longue / moyenne
-  
-  #confort - longue et puissante tres longue avec des cheveaux & prix & 5 portes
-  
-  #classic - le reste (5 portes, courte / moyenne / longue, puissance moyenne)
+  #citadine #sportive #familiale #confort #classic 
 
-definir_categorie <- function(puissance, longueur, nbplaces, nbportes) {
-  ifelse(puissance <= 150 & longueur %in% c("courte", "moyenne"), "citadine",
-         ifelse(puissance > 200 & nbportes == 3, "sportive",
-                ifelse(nbplaces > 5 | (puissance <= 200 & longueur %in% c("longue", "moyenne", "très longue")), "familiale",
-                       ifelse(longueur %in% c("longue", "moyenne", "très longue") & puissance > 200 & nbportes == 5, "confort",
-                              "classic"))))
+# Installer et charger le package cluster
+install.packages("cluster")
+library(cluster)
+
+# Fonction de catégorisation
+definir_categorie_cluster <- function(data) {
+  # Préparer les données
+  data_prep <- data[, c("puissance", "longueur", "nbplaces", "nbportes","malusbonus","rejetsco2","coutenergie")]
+  
+  # Convertir longueur en facteur si ce n'est pas déjà fait
+  if (!is.factor(data_prep$longueur)) {
+    data_prep$longueur <- factor(data_prep$longueur)
+  }
+  
+  # Normaliser les variables numériques
+  data_norm <- scale(data_prep[, c("puissance", "nbplaces", "nbportes","malusbonus","rejetsco2","coutenergie")])
+  
+  # Clustering hiérarchique
+  hc <- hclust(dist(data_norm), method = "ward.D")
+  
+  # Découper les clusters en 5 catégories
+  nb_cluster <- 5
+  coupes <- cutree(hc, k = nb_cluster)
+  
+  # Mapper les clusters aux catégories
+  mapping_clusters_categories <- function(cluster) {
+    if (cluster == 1) {
+      return("citadine")
+    } else if (cluster == 2) {
+      return("sportive")
+    } else if (cluster == 3) {
+      return("familiale")
+    } else if (cluster == 4) {
+      return("confort")
+    } else if (cluster == 5) {
+      return("longue")
+    }
+  }
+  
+  # Ajouter la colonne "categorie" sans perdre les colonnes existantes
+  data$categorie <- sapply(coupes, mapping_clusters_categories)
+  
+  # Renvoyer les données avec la catégorie ajoutée
+  return(data)
 }
 
 
-# Appliquer la fonction aux différents datasets
-catalogue$categorie <- with(catalogue, definir_categorie(puissance, longueur, nbplaces, nbportes))
-immatriculation$categorie <- with(immatriculation, definir_categorie(puissance, longueur, nbplaces, nbportes))
-clientImmat$categorie <- with(clientImmat, definir_categorie(puissance, longueur, nbplaces, nbportes))
+
+
+# Appliquer la fonction aux datasets
+catalogue <- definir_categorie_cluster(catalogue)
+
+# Convertir toutes les valeurs de la colonne "marque" en minuscules pour la fusion insensible à la casse
+immatriculation$marque <- tolower(immatriculation$marque)
+catalogue$marque <- tolower(catalogue$marque)
+clientImmat$marque <- tolower(clientImmat$marque)
+
+# Fusionner en ignorant la casse
+immatriculation <- merge(immatriculation, catalogue[, c("marque", "categorie")], by = "marque", all.x = TRUE)
+clientImmat <- merge(clientImmat, catalogue[, c("marque", "categorie")], by = "marque", all.x = TRUE)
+
 
 # Vérifier le nombre d'éléments dans chaque catégorie
-table(catalogue$categorie)
-table(immatriculation$categorie)
-table(clientImmat$categorie)
+print(table(catalogue$categorie))
+print(table(immatriculation$categorie))
+print(table(clientImmat$categorie))
