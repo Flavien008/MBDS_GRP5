d250acd0eb22b092b777b4cc9cbddc012548f863
MiranaIns <89468422+MiranaIns@users.noreply.github.com>
2024-05-23 00:57:49 +0300

    Fix Application Modèle

--- AnalyseR/ApplicationModèle.R
+++ AnalyseR/ApplicationModèle.R
@@ -3,10 +3,6 @@
 # APPLICATION DU MODELE SUR LES DONNEES MARKETING #
 #----------------------------------------------------------------#
 
-# Chargement des données marketing
-# Supposons que les données marketing soient dans un dataframe nommé marketing
-# marketing <- read.csv("path_to_marketing_data.csv")
-
 # Prétraitement des données marketing
 marketing$sexe <- as.factor(marketing$sexe)
 marketing$situation_familiale <- as.factor(marketing$situation_familiale)
